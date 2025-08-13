34758a64da3e5627f59aa0f5147a36117945054b
Rishi Rajan <rishi.r@cognizant.com>
2025-08-13 21:34:08 +0530

    Admin Home page changes

--- src/components/admin/AdminHome.vue
+++ src/components/admin/AdminHome.vue
@@ -1,12 +1,31 @@
 <template>
-  <div>
-    <h1>Admin Home</h1>
-    <nav>
-      <router-link to="/assign-evaluation">Assign Evaluation</router-link>
-      <router-link to="/upload-questions">Upload Questions</router-link>
-      <router-link to="/user-management">User Management</router-link>
-    </nav>
-  </div>
+  <v-container>
+    <v-row justify="center" class="my-4">
+      <v-col cols="12" class="text-center">
+        <h1>Admin Home</h1>
+      </v-col>
+    </v-row>
+    <v-row justify="space-around">
+      <v-col cols="auto">
+        <v-btn to="/assign-evaluation" color="primary" large>
+          <v-icon left>mdi-account-plus</v-icon>
+          Assign Evaluation
+        </v-btn>
+      </v-col>
+      <v-col cols="auto">
+        <v-btn to="/upload-questions" color="primary" large>
+          <v-icon left>mdi-upload</v-icon>
+          Upload Questions
+        </v-btn>
+      </v-col>
+      <v-col cols="auto">
+        <v-btn to="/user-management" color="primary" large>
+          <v-icon left>mdi-account-group</v-icon>
+          User Management
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script setup>
